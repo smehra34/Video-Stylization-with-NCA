bdb72b0840f7fa6ee5bb8602c6eb3b864b977407
Somesh Mehra <someshmehra99@gmail.com>
2024-05-16 19:48:12 +0200

    Bugs mostly fixed in experiment script

--- ExtraChannels/experiments.py
+++ ExtraChannels/experiments.py
@@ -37,7 +37,6 @@ def setup_args():
     parser.add_argument('--exp_name', type=str, default='no-positional-encoding-with-motion-loss', help='Name of the experiment')
     parser.add_argument('--img_size', type=int, nargs=2, default=[256, 256], help='Image size during training')
     parser.add_argument('--video_length', type=int, default=10, help='Output video length in seconds')
-    parser.add_argument('--exp_description', type=str, default='no-positional-encoding-with-motion-loss', help='Experiment description')
     parser.add_argument('--style_name', type=str, default='starry-night', help='Name of the style image')
     parser.add_argument('--style_img_ext', type=str, default='jpg', help='Extension of the style image')
     parser.add_argument('--target_appearance_2_path', type=str, default='data/Reference/mr-bean.gif', help='Path to the second target appearance reference')
@@ -51,14 +50,14 @@ def setup_args():
     parser.add_argument('--nca_c_in', type=int, default=12, help='Input channel of DyNCA')
     parser.add_argument('--nca_fc_dim', type=int, default=96, help='Dimensionality of update MLP in DyNCA')
     parser.add_argument('--nca_seed_mode', type=str, default='zeros', help='Seed initialization mode')
-    parser.add_argument('--nca_padding_mode', type=str, default='replicate', help='Padding mode in perception')
+    parser.add_argument('--nca_padding_mode', type=str, default='circular', help='Padding mode in perception')
     parser.add_argument('--nca_pos_emb', type=str, default=None, help='Positional encoding type')
     parser.add_argument('--nca_perception_scales', type=int, nargs='+', default=[0], help='Perception scales for NCA')
     parser.add_argument('--nca_base_num_steps', type=int, default=24, help='Base number of steps for NCA')
 
 
     # motion loss parameters
-    
+
     parser.add_argument('--motion_img_size', type=int, nargs=2, default=[256, 256], help='Image size for motion field')
     # motion_vector_field_name: 'circular', 'random', 'horizontal', 'vertical', 'diagonal'
     parser.add_argument('--motion_vector_field_name', type=str, default='circular', help='Name of the motion vector field')
@@ -81,7 +80,7 @@ def setup_args():
 
     parser.add_argument('--auxillary_loss_weight', type=float, default=10.0, help='Weight for auxiliary loss')
     # auxillary_loss_type: 'MSE', 'OT'
-    parser.add_argument('--auxillary_loss_type', type=str, default='MSE', help='Type of auxiliary loss')
+    parser.add_argument('--auxillary_loss_type', type=str, default='VGG', help='Type of auxiliary loss')
 
     # overflow loss parameters
     parser.add_argument('--overflow_loss_weight', type=float, default=1000.0, help='Weight for overflow loss')
@@ -99,13 +98,16 @@ def setup_args():
 def main():
     args = setup_args()
     experiment_index = get_next_experiment_index('experiments')
+    args.exp_description = f"{args.exp_name}_{experiment_index}"
+
     wandb.init(
         # set the wandb project where this run will be logged
-        project='Video_Stylization_with_NCA', 
-        entity='aliraed-benmustapha',
-        config = {'experiment_index': experiment_index, **vars(args)}
+        project='Condiitional-NCA',
+        entity='video_stylization_with_NCAs',
+        config = {'experiment_index': experiment_index, **vars(args)},
+        name = args.exp_description
         )
-    wandb.run.name = args.exp_description
+
     DEVICE = torch.device(args.DEVICE)
     
     # Load the style image
@@ -129,7 +131,7 @@ def main():
     ###### setup the DyNCA model for training ######
     nca_size_x, nca_size_y = int(args.img_size[0]), int(args.img_size[1])
 
-    try:    
+    try:
         nca_perception_scales = args.nca_perception_scales
     except:
         nca_perception_scales = [0]
@@ -183,17 +185,17 @@ def main():
             np.random.seed(i + 424)
             torch.manual_seed(i + 424)
             torch.cuda.manual_seed_all(i + 424)
-        
+
             aux_img_ids = np.random.choice(target_reference_img.size(0), args.batch_size, replace=True)
             aux_imgs = [target_reference_img[img].unsqueeze(0) for img in aux_img_ids]
             aux_imgs = torch.concat(aux_imgs)
             aux_imgs_vis = (aux_imgs + 1.0) / 2.0
             input_dict['auxillary_image_list'] = [aux_imgs]  # 0,1
-        
+
             aux_edges = [target_reference_edges[img].unsqueeze(0) for img in aux_img_ids]
             aux_edges = torch.concat(aux_edges)
             aux_edges_vis = (aux_edges + 1.0) / 2.0
-        
+
             with torch.no_grad():
                 batch_idx = np.random.choice(args.nca_pool_size, args.batch_size, replace=False)
                 input_states = nca_pool[batch_idx]
@@ -202,111 +204,120 @@ def main():
                     seed_injection = True
                     seed_inject = nca_model.seed(1, size=(nca_size_x, nca_size_y))
                     input_states[:1] = seed_inject[:1]
-                
+
                 input_states = torch.cat((input_states, aux_edges), 1)
                 '''Get the image before NCA iteration for computing optic flow'''
                 nca_states_before, nca_features_before = nca_model.forward_nsteps(input_states, step_n=1)
                 z_before_nca = nca_features_before
                 image_before_nca = z_before_nca
-        
+
             step_n = np.random.randint(nca_min_steps, nca_max_steps)
             input_dict['step_n'] = step_n
             nca_states_after, nca_features_after = nca_model.forward_nsteps(input_states, step_n)
-        
+
             z = nca_features_after
             generated_image = z
             with torch.no_grad():
                 generated_image_vis = generated_image.clone()
                 generated_image_vis = (generated_image_vis + 1.0) / 2.0
-        
+
             image_after_nca = generated_image.clone()
-        
+
             '''Construct input dictionary for loss computation'''
             input_dict['generated_image_list'] = [generated_image]
             input_dict['generated_image_before_nca'] = image_before_nca
             input_dict['generated_image_after_nca'] = image_after_nca
-        
-            
-        
+
+
+
             input_dict['nca_state'] = nca_states_after
-        
-            if i % args.save_every == 0:
-                batch_loss, batch_loss_log_dict, summary = DynamicTextureLoss(input_dict, return_summary=True)
-                print('batch_loss', batch_loss)
-                print('batch_loss_log_dict', batch_loss_log_dict)
-                print('summary', summary)
-                # Assuming 'batch_loss', 'batch_loss_log_dict', and 'summary' are obtained during your training loop
-            wandb.log({
-                "batch_loss": batch_loss.item(),  # Log the scalar value of batch loss
-                **batch_loss_log_dict,            # Expand the dictionary to log each component separately
-            })
 
-            # Log the generated and reference images
-            img_show = save_train_image(generated_image_vis.detach().cpu().numpy(), None, return_img = True)
-            ref_img_show = save_train_image(aux_imgs_vis.detach().cpu().numpy(), None, return_img = True)
-            wandb.log({"Generated Image": wandb.Image(img_show)})
-            wandb.log({"Reference Image": wandb.Image(ref_img_show)})
-           
-            # Additionally, to log images stored in 'summary':
-            if 'vector_field_motion-generated_video_flow' in summary:
-                # Extract the numpy array containing video flow
-                video_flow_data = summary['vector_field_motion-generated_video_flow']
-                
-                # Assuming the data shape is (num_images, channels, height, width)
-                # We'll log the first image in the batch for simplicity
-                if video_flow_data.ndim == 4 and video_flow_data.shape[1] == 3:  # Check if it is in the format (N, C, H, W)
-                    # Convert from (N, C, H, W) to (N, H, W, C) for the first image
-                    first_image = video_flow_data[0].transpose(1, 2, 0)  # This converts C, H, W to H, W, C
-                    video_flow_img = wandb.Image(first_image)  # Create a wandb Image from numpy array
-                    wandb.log({"Generated Video Flow": video_flow_img})  # Log the image to wandb
-           
-            # Log generated and target flow vector fields
-            if 'vector_field_motion-generated_flow_vector_field' in summary:
-                # Directly log the PIL image, assuming it is correctly formatted
-                generated_flow_vector_field_img = wandb.Image(summary['vector_field_motion-generated_flow_vector_field'])
-                wandb.log({"Generated Flow Vector Field": generated_flow_vector_field_img})
+            batch_loss, batch_loss_log_dict, summary = DynamicTextureLoss(input_dict, return_summary=True)
 
-            if 'vector_field_motion-target_flow_vector_field' in summary:
-                # Directly log the PIL image, assuming it is correctly formatted
-                target_flow_vector_field_img = wandb.Image(summary['vector_field_motion-target_flow_vector_field'])
-                wandb.log({"Target Flow Vector Field": target_flow_vector_field_img})
-            
-            else:
-                batch_loss, batch_loss_log_dict, _ = DynamicTextureLoss(input_dict, return_summary=False)
-                summary = {}
+            #if i % args.save_every == 0:
+        #        batch_loss, batch_loss_log_dict, summary = DynamicTextureLoss(input_dict, return_summary=True)
+    #            print('batch_loss', batch_loss)
+#                print('batch_loss_log_dict', batch_loss_log_dict)
+#                print('summary', summary)
+
             with torch.no_grad():
                 batch_loss.backward()
                 if torch.isnan(batch_loss):
                     print('Loss is NaN. Train Failed. Exit.')
                     exit()
-        
+
                 for p_name, p in nca_model.named_parameters():
                     p.grad /= (p.grad.norm() + 1e-8)  # normalize gradients
-                    
+
                 optimizer.step()
                 optimizer.zero_grad()
                 lr_scheduler.step()
-        
+
                 nca_pool[batch_idx] = nca_states_after[:, :12, :, :]
-            
+
+                display_dict = copy.deepcopy(batch_loss_log_dict)
+                    # Assuming 'batch_loss', 'batch_loss_log_dict', and 'summary' are obtained during your training loop
+                wandb.log({
+                    "batch_loss": batch_loss.item(),  # Log the scalar value of batch loss
+                    **display_dict,            # Expand the dictionary to log each component separately
+                })
+
+                # Log the generated and reference images
+                img_show = save_train_image(generated_image_vis.detach().cpu().numpy(), None, return_img = True)
+                ref_img_show = save_train_image(aux_imgs_vis.detach().cpu().numpy(), None, return_img = True)
+                wandb.log({"Generated Image": wandb.Image(img_show)})
+                wandb.log({"Reference Image": wandb.Image(ref_img_show)})
+
+                # Additionally, to log images stored in 'summary':
+                if 'vector_field_motion-generated_video_flow' in summary:
+                    # Extract the numpy array containing video flow
+                    video_flow_data = summary['vector_field_motion-generated_video_flow']
+
+                    # Assuming the data shape is (num_images, channels, height, width)
+                    # We'll log the first image in the batch for simplicity
+                    if video_flow_data.ndim == 4 and video_flow_data.shape[1] == 3:  # Check if it is in the format (N, C, H, W)
+                        # Convert from (N, C, H, W) to (N, H, W, C) for the first image
+                        first_image = video_flow_data[0].transpose(1, 2, 0)  # This converts C, H, W to H, W, C
+                        video_flow_img = wandb.Image(first_image)  # Create a wandb Image from numpy array
+                        wandb.log({"Generated Video Flow": video_flow_img})  # Log the image to wandb
+
+                # Log generated and target flow vector fields
+                if 'vector_field_motion-generated_flow_vector_field' in summary:
+                    # Directly log the PIL image, assuming it is correctly formatted
+                    generated_flow_vector_field_img = wandb.Image(summary['vector_field_motion-generated_flow_vector_field'])
+                    wandb.log({"Generated Flow Vector Field": generated_flow_vector_field_img})
+
+                if 'vector_field_motion-target_flow_vector_field' in summary:
+                    # Directly log the PIL image, assuming it is correctly formatted
+                    target_flow_vector_field_img = wandb.Image(summary['vector_field_motion-target_flow_vector_field'])
+                    wandb.log({"Target Flow Vector Field": target_flow_vector_field_img})
+
+                #else:
+            #        batch_loss, batch_loss_log_dict, _ = DynamicTextureLoss(input_dict, return_summary=False)
+            #        summary = {}
+
+
     except (KeyboardInterrupt, torch.cuda.OutOfMemoryError) as e:
         print(e)
         print('Saving latest model checkpoint...')
     model_save_path = f'experiments/experiment_{experiment_index}/models/'
     ensure_dir(model_save_path)
     torch.save(nca_model, model_save_path + f'model_{i}.pth')
+
+    video_save_path = f'experiments/experiment_{experiment_index}/videos/'
+    ensure_dir(video_save_path)
     ## Generate Videos
     ## CORGI
-    save_video(f"experminets/experiment{experiment_index}/corgi", target_vid_path='data/Reference/corgi.gif', size_factor=2.0, step_n=int(args.nca_base_num_steps), steps_per_frame=10, is_style_image=False, nca_model=nca_model, nca_size_x=nca_size_x, nca_size_y=nca_size_y, DEVICE=DEVICE)
+    save_video(f"{video_save_path}/corgi", target_vid_path='data/Reference/corgi.gif', size_factor=2.0, step_n=int(args.nca_base_num_steps), steps_per_frame=10, is_style_image=False, nca_model=nca_model, nca_size_x=nca_size_x, nca_size_y=nca_size_y, DEVICE=DEVICE, autoplay=False)
     ##GOT
-    save_video(f"experminets/experiment{experiment_index}/got", target_vid_path='data/Reference/got.gif', size_factor=3.0, step_n=int(args.nca_base_num_steps), steps_per_frame=2, is_style_image=False, nca_model=nca_model, nca_size_x=nca_size_x, nca_size_y=nca_size_y, DEVICE=DEVICE)
+    save_video(f"{video_save_path}/got", target_vid_path='data/Reference/got.gif', size_factor=3.0, step_n=int(args.nca_base_num_steps), steps_per_frame=2, is_style_image=False, nca_model=nca_model, nca_size_x=nca_size_x, nca_size_y=nca_size_y, DEVICE=DEVICE, autoplay=False)
     ##MR BEAN
-    save_video(f"experminets/experiment{experiment_index}/mr_bean", target_vid_path='data/Reference/mr-bean.gif', size_factor=2.0, step_n=int(args.nca_base_num_steps), steps_per_frame=2, is_style_image=False, nca_model=nca_model, nca_size_x=nca_size_x, nca_size_y=nca_size_y, DEVICE=DEVICE)
+    save_video(f"{video_save_path}/mr_bean", target_vid_path='data/Reference/mr-bean.gif', size_factor=2.0, step_n=int(args.nca_base_num_steps), steps_per_frame=2, is_style_image=False, nca_model=nca_model, nca_size_x=nca_size_x, nca_size_y=nca_size_y, DEVICE=DEVICE, autoplay=False)
     ##STARRY NIGHT
-    save_video(f"experminets/experiment{experiment_index}/{args.style_name}", target_vid_path='data/Reference/{style_name}.gif', size_factor=2.0, step_n=int(args.nca_base_num_steps), steps_per_frame=2, is_style_image=False, nca_model=nca_model, nca_size_x=nca_size_x, nca_size_y=nca_size_y, DEVICE=DEVICE)
+    save_video(f"{video_save_path}/{args.style_name}", target_vid_path=f'data/Reference/{args.style_name}.gif', size_factor=2.0, step_n=int(args.nca_base_num_steps), steps_per_frame=2, is_style_image=False, nca_model=nca_model, nca_size_x=nca_size_x, nca_size_y=nca_size_y, DEVICE=DEVICE, autoplay=False)
     # Raed Webcam
-    save_video(f"experminets/experiment{experiment_index}/raed_webcam", target_vid_path='data/Reference/raed.mp4', size_factor=2.0, step_n=int(args.nca_base_num_steps), steps_per_frame=2, is_style_image=False, nca_model=nca_model, nca_size_x=nca_size_x, nca_size_y=nca_size_y, DEVICE=DEVICE)
+    save_video(f"{video_save_path}/raed_webcam", target_vid_path='data/Reference/raed.mp4', size_factor=2.0, step_n=int(args.nca_base_num_steps), steps_per_frame=2, is_style_image=False, nca_model=nca_model, nca_size_x=nca_size_x, nca_size_y=nca_size_y, DEVICE=DEVICE, autoplay=False)
     # Black background
-    save_video(f"experminets/experiment{experiment_index}/black_background", target_vid_path='data/Reference/black_background.mp4', size_factor=2.0, step_n=int(args.nca_base_num_steps), steps_per_frame=2, is_style_image=False, nca_model=nca_model, nca_size_x=nca_size_x, nca_size_y=nca_size_y, DEVICE=DEVICE)
+    save_video(f"{video_save_path}/black_background", target_vid_path='data/Reference/black_background.mp4', size_factor=2.0, step_n=int(args.nca_base_num_steps), steps_per_frame=2, is_style_image=False, nca_model=nca_model, nca_size_x=nca_size_x, nca_size_y=nca_size_y, DEVICE=DEVICE, autoplay=False)
 if __name__ == '__main__':
     main()
--- ExtraChannels/helper.py
+++ ExtraChannels/helper.py
@@ -35,7 +35,7 @@ def save_setup_images(target_reference_img, target_reference_edges, target_appea
     experiment_index = get_next_experiment_index(base_directory)
     directory = os.path.join(base_directory, f'experiment_{experiment_index}', 'setup_images')
     ensure_dir(directory)
-    
+
     # Process and save the target reference image
     # Normalize from [-1, 1] to [0, 255] for color images
     img = (target_reference_img[0].detach().cpu().permute(1, 2, 0).numpy() + 1) * 127.5
@@ -46,38 +46,37 @@ def save_setup_images(target_reference_img, target_reference_edges, target_appea
     edges = target_reference_edges[0][1].detach().cpu().numpy()
     edges_normalized = (edges - edges.min()) / (edges.max() - edges.min()) * 255
     Image.fromarray(edges_normalized.astype(np.uint8), mode='L').save(os.path.join(directory, 'target_reference_edges.png'))
-    
+
     # Process and save the target appearance image
     # Normalize from [-1, 1] to [0, 255] for color images
     appearance = (target_appearance_img[0].detach().cpu().permute(1, 2, 0).numpy() + 1) * 127.5
     Image.fromarray(appearance.astype(np.uint8)).save(os.path.join(directory, 'target_appearance_image.png'))
 
-def save_video(video_name, target_vid_path, size_factor=1.0, step_n=8, steps_per_frame=1, is_style_image=False, nca_model=None, nca_size_x=256, nca_size_y=256, DEVICE='cuda'):
+def save_video(video_name, target_vid_path, size_factor=1.0, step_n=8, steps_per_frame=1, is_style_image=False, nca_model=None, nca_size_x=256, nca_size_y=256, DEVICE='cuda', autoplay=True):
     target_vid = preprocess_video(target_vid_path,
                                     img_size=(int(nca_size_x * size_factor), int(nca_size_y * size_factor)))  # [C, T, H, W]
-    
+
 
     target_vid = target_vid.permute(1,0,2,3).to(DEVICE)
-    
-    with VideoWriter(filename=f"{video_name}.mp4", fps=30, autoplay=True) as vid, torch.no_grad():
+
+    with VideoWriter(filename=f"{video_name}.mp4", fps=30, autoplay=autoplay) as vid, torch.no_grad():
         h = nca_model.seed(1, size=(int(nca_size_x * size_factor), int(nca_size_y * size_factor)))
 
-        
+
 
         for frame in tqdm(range(target_vid.size(0)),  desc="Making the video..."):
             for k in range(int(steps_per_frame)):
                 f = 1 if not is_style_image else 90
                 for i in range(f):
-    
+
                     h = torch.cat((h, target_vid[frame].unsqueeze(0)), 1)
                     nca_state, nca_feature = nca_model.forward_nsteps(h, step_n)
-    
+
                     z = nca_feature
                     h = nca_state[:, :-3, :, :]
-    
+
                     img = z.detach().cpu().numpy()[0]
                     img = img.transpose(1, 2, 0)
                     img = np.clip(img, -1.0, 1.0)
                     img = (img + 1.0) / 2.0
                     vid.add(img)
-            
