2c566e4444d0657cc70e7060f517b58dc23c8c97
Ahmad Jarrar <ahmad.jarrar1@outlook.com>
2024-09-14 20:49:27 +0200

    Added experiments

--- ConditioneDyNCA/exp.sh
+++ ConditioneDyNCA/exp.sh
@@ -5,18 +5,18 @@ WANDB_API_KEY=55cda04bc8e196e576724e5be8baeebf6b8bd40c
 
 # Test Experiments
 
-python train.py --style_name comic\
-    --exp_name server_test_run \
-    --no_wandb \
-    --no_index \
-    --target_images full \
-    --batch_size 2 \
-    --vector_field_motion_loss_weight 6.0 \
-    --appearance_loss_weight 6.0 \
-    --auxillary_loss_weight .1 \
-    --overflow_loss_weight 1000.0
+# python train.py --style_name comic\
+#     --exp_name server_test_run \
+#     --no_wandb \
+#     --no_index \
+#     --target_images full \
+#     --batch_size 2 \
+#     --vector_field_motion_loss_weight 6.0 \
+#     --appearance_loss_weight 6.0 \
+#     --auxillary_loss_weight .1 \
+#     --overflow_loss_weight 1000.0
 
-python generate_videos.py --exp_description server_test_run --style_name comic
+# python generate_videos.py --exp_description server_test_run --style_name comic
 
 
 # ---------------------------------------------
@@ -25,80 +25,140 @@ python generate_videos.py --exp_description server_test_run --style_name comic
 
 # # Full Dataset
 
-# python train.py --style_name comic\
-#     --exp_name comic-baseline_full \
-#     --target_images full \
-#     --batch_size 4 \
-#     --vector_field_motion_loss_weight 6.0 \
-#     --appearance_loss_weight 6.0 \
-#     --auxillary_loss_weight .1 \
-#     --overflow_loss_weight 1000.0
+python train.py --style_name comic\
+    --exp_name comic-baseline_full \
+    --no_index \
+    --target_images full \
+    --batch_size 4 \
+    --vector_field_motion_loss_weight 6.0 \
+    --appearance_loss_weight 6.0 \
+    --auxillary_loss_weight .1 \
+    --overflow_loss_weight 1000.0
+
+python generate_videos.py --exp_description comic-baseline_full --style_name comic
 
 
-# python train.py --style_name starry-night\
-#     --exp_name starry-night-baseline_full \
-#     --target_images full \
-#     --batch_size 4 \
-#     --vector_field_motion_loss_weight 6.0 \
-#     --appearance_loss_weight 6.0 \
-#     --auxillary_loss_weight .1 \
-#     --overflow_loss_weight 1000.0
+python train.py --style_name starry-night\
+    --exp_name starry-night-baseline_full \
+    --no_index \
+    --target_images full \
+    --batch_size 4 \
+    --vector_field_motion_loss_weight 6.0 \
+    --appearance_loss_weight 6.0 \
+    --auxillary_loss_weight .1 \
+    --overflow_loss_weight 1000.0
 
-# python train.py --style_name pencil-sketch\
-#     --exp_name pencil-sketch-baseline_full \
-#     --target_images full \
-#     --batch_size 4 \
-#     --vector_field_motion_loss_weight 6.0 \
-#     --appearance_loss_weight 6.0 \
-#     --auxillary_loss_weight .1 \
-#     --overflow_loss_weight 1000.0
+python generate_videos.py --exp_description starry-night-baseline_full --style_name starry-night
 
-# python train.py --style_name japanese-wave-art\
-#     --exp_name japanese-wave-art-baseline_full \
-#     --target_images full \
-#     --batch_size 4 \
-#     --vector_field_motion_loss_weight 6.0 \
-#     --appearance_loss_weight 6.0 \
-#     --auxillary_loss_weight .1 \
-#     --overflow_loss_weight 1000.0
+python train.py --style_name pencil-sketch\
+    --exp_name pencil-sketch-baseline_full \
+    --no_index \
+    --target_images full \
+    --batch_size 4 \
+    --vector_field_motion_loss_weight 6.0 \
+    --appearance_loss_weight 6.0 \
+    --auxillary_loss_weight .1 \
+    --overflow_loss_weight 1000.0
+
+python generate_videos.py --exp_description pencil-sketch-baseline_full --style_name pencil-sketch
+
+# # 5 Images Dataset
+
+python train.py --style_name comic\
+    --exp_name comic-baseline_5_images \
+    --no_index \
+    --target_images 5_images \
+    --batch_size 4 \
+    --vector_field_motion_loss_weight 6.0 \
+    --appearance_loss_weight 6.0 \
+    --auxillary_loss_weight .1 \
+    --overflow_loss_weight 1000.0
+
+python generate_videos.py --exp_description comic-baseline_5_images --style_name comic
+
+python train.py --style_name starry-night\
+    --exp_name starry-night-baseline_5_images \
+    --no_index \
+    --target_images 5_images \
+    --batch_size 4 \
+    --vector_field_motion_loss_weight 6.0 \
+    --appearance_loss_weight 6.0 \
+    --auxillary_loss_weight .1 \
+    --overflow_loss_weight 1000.0
+
+python generate_videos.py --exp_description starry-night-baseline_5_images --style_name starry-night
+
+python train.py --style_name pencil-sketch\
+    --exp_name pencil-sketch-baseline_5_images \
+    --no_index \
+    --target_images 5_images \
+    --batch_size 4 \
+    --vector_field_motion_loss_weight 6.0 \
+    --appearance_loss_weight 6.0 \
+    --auxillary_loss_weight .1 \
+    --overflow_loss_weight 1000.0
+
+python generate_videos.py --exp_description pencil-sketch-baseline_5_images --style_name pencil-sketch
+
+python train.py --style_name japanese-wave-art\
+    --exp_name japanese-wave-art-baseline_5_images \
+    --no_index \
+    --target_images 5_images \
+    --batch_size 4 \
+    --vector_field_motion_loss_weight 6.0 \
+    --appearance_loss_weight 6.0 \
+    --auxillary_loss_weight .1 \
+    --overflow_loss_weight 1000.0
+
+python generate_videos.py --exp_description japanese-wave-art-baseline_5_images --style_name japanese-wave-art
 
 
-# # 10 Images Dataset
+# # 2 Images Dataset
 
-# python train.py --style_name comic\
-#     --exp_name comic-baseline_10_images \
-#     --target_images 10_images \
-#     --batch_size 4 \
-#     --vector_field_motion_loss_weight 6.0 \
-#     --appearance_loss_weight 6.0 \
-#     --auxillary_loss_weight .1 \
-#     --overflow_loss_weight 1000.0
+python train.py --style_name comic\
+    --exp_name comic-baseline_2_images \
+    --no_index \
+    --target_images 2_images \
+    --batch_size 4 \
+    --vector_field_motion_loss_weight 6.0 \
+    --appearance_loss_weight 6.0 \
+    --auxillary_loss_weight .1 \
+    --overflow_loss_weight 1000.0
 
+python generate_videos.py --exp_description comic-baseline_2_images --style_name comic
 
-# python train.py --style_name starry-night\
-#     --exp_name starry-night-baseline_10_images \
-#     --target_images 10_images \
-#     --batch_size 4 \
-#     --vector_field_motion_loss_weight 6.0 \
-#     --appearance_loss_weight 6.0 \
-#     --auxillary_loss_weight .1 \
-#     --overflow_loss_weight 1000.0
+python train.py --style_name starry-night\
+    --exp_name starry-night-baseline_2_images \
+    --no_index \
+    --target_images 2_images \
+    --batch_size 4 \
+    --vector_field_motion_loss_weight 6.0 \
+    --appearance_loss_weight 6.0 \
+    --auxillary_loss_weight .1 \
+    --overflow_loss_weight 1000.0
 
-# python train.py --style_name pencil-sketch\
-#     --exp_name pencil-sketch-baseline_10_images \
-#     --target_images 10_images \
-#     --batch_size 4 \
-#     --vector_field_motion_loss_weight 6.0 \
-#     --appearance_loss_weight 6.0 \
-#     --auxillary_loss_weight .1 \
-#     --overflow_loss_weight 1000.0
+python generate_videos.py --exp_description starry-night-baseline_2_images --style_name starry-night
 
-# python train.py --style_name japanese-wave-art\
-#     --exp_name japanese-wave-art-baseline_10_images \
-#     --target_images 10_images \
-#     --batch_size 4 \
-#     --vector_field_motion_loss_weight 6.0 \
-#     --appearance_loss_weight 6.0 \
-#     --auxillary_loss_weight .1 \
-#     --overflow_loss_weight 1000.0
+python train.py --style_name pencil-sketch\
+    --exp_name pencil-sketch-baseline_2_images \
+    --no_index \
+    --target_images 2_images \
+    --batch_size 4 \
+    --vector_field_motion_loss_weight 6.0 \
+    --appearance_loss_weight 6.0 \
+    --auxillary_loss_weight .1 \
+    --overflow_loss_weight 1000.0
 
+python generate_videos.py --exp_description pencil-sketch-baseline_2_images --style_name pencil-sketch
+
+python train.py --style_name japanese-wave-art\
+    --exp_name japanese-wave-art-baseline_2_images \
+    --no_index \
+    --target_images 2_images \
+    --batch_size 4 \
+    --vector_field_motion_loss_weight 6.0 \
+    --appearance_loss_weight 6.0 \
+    --auxillary_loss_weight .1 \
+    --overflow_loss_weight 1000.0
+
+python generate_videos.py --exp_description japanese-wave-art-baseline_2_images --style_name japanese-wave-art
